da47bfcfe13a8d28554724ca860614f1874d34f9
truantmuse <shanbin@vipkid.com.cn>
2020-05-12 13:25:18 +0800

    start.sh

--- start.sh
+++ start.sh
@@ -7,8 +7,8 @@ python bert_qa.py \
   --do_train \
   --do_eval \
   --do_lower_case \
-  --train_file DATA_DIR/cmrc2018_train.json \
-  --predict_file DATA_DIR/cmrc2018_dev.json \
+  --train_file $DATA_DIR/cmrc2018_train.json \
+  --predict_file $DATA_DIR/cmrc2018_dev.json \
   --per_gpu_train_batch_size 32 \
   --learning_rate 3e-5 \
   --num_train_epochs 3.0 \
