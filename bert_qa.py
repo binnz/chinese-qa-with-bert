19d115fa402106886d433dab355c0ef9c4884ec6
truantmuse <shanbin@vipkid.com.cn>
2020-05-12 11:45:51 +0800

    bert-base-chinese

--- bert_qa.py
+++ bert_qa.py
@@ -461,7 +461,7 @@ def main():
     config = config_class.from_pretrained(args.config_name if args.config_name else args.model_name_or_path)
     tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name if args.tokenizer_name else args.model_name_or_path, do_lower_case=args.do_lower_case)
 
-    model = model_class.from_pretrained("bert-based-chinese", config=config)
+    model = model_class.from_pretrained("bert-base-chinese", config=config)
     if args.local_rank == 0:
         torch.distributed.barrier()  # Make sure only the first process in distributed training will download model & vocab
 
